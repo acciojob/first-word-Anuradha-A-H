530afc49e35a3a8385917f94664bc90d7d630d19
Anuradha-A-H <119663653+Anuradha-A-H@users.noreply.github.com>
2024-07-29 21:13:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function firstWord(s) {
   // your code here
-	let arr = s.split(' ');
-	 let str = arr.length()>0?arr[0]:'';
+	let arr = s.trim().split(' ');
+	 let str = arr.length>0? arr[0]:'';
 	return str;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
